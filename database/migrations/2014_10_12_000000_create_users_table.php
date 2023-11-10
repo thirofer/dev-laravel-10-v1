599c8aefbfde7da35646e3decac700f2f3d5078b
Thiago <thiago.rodrigues@eframetec.com.br>
2023-11-10 20:39:47 -0300

    Changing migrations to match my project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->id('id');
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('login', 20);
+            $table->integer('active');
+            $table->integer('insert_by');
+            $table->timestamps('insert_at');
+            $table->integer('update_by')->nullable();
+            $table->timestamps('update_at');
+            $table->integer('delete_by')->nullable();
+            $table->timestamps('delete_at');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
