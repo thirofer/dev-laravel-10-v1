565f36f39ff581945b2efbd85170f4010c8c9807
Thiago <thirofer@gmail.com>
2023-11-10 20:43:06 -0300

    Updating User migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id');
+            $table->bigIncrements('id');
             $table->string('name', 100);
             $table->string('email', 100)->unique();
             $table->string('login', 20);
