75b42780021e55815ac594774a52ad5e407cead6
Thiago <thirofer@gmail.com>
2023-11-14 17:33:33 -0300

    Settings page fixes

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,31 +9,25 @@
                         <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="false">Profile</a>
                     </li>
                 </ul>
-                <form>
+                <form action="{{ route('settings.index') }}" method='post'>
                     <div class="row mt-5 align-items-center">
                         <div class="col-md-3 text-center mb-5">
                             <div class="avatar avatar-xl">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="..." class="avatar-img rounded-circle" />
+                                <img src="https://t3.ftcdn.net/jpg/01/36/49/90/360_F_136499077_xp7bSQB4Dx13ktQp0OYJ5ricWXhiFtD2.jpg" width="150px" height="150px" alt="..." class="avatar-img rounded-5" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="row align-items-center">
                                 <div class="col-md-7">
-                                    <h4 class="mb-1">Brown, Asher</h4>
+                                    <h4 class="mb-1">Mr. Robot</h4>
                                     <p class="small mb-3"><span class="badge badge-dark">New York, USA</span></p>
                                 </div>
                             </div>
                             <div class="row mb-4">
                                 <div class="col-md-7">
                                     <p class="text-muted">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit nisl ullamcorper, rutrum metus in, congue lectus. In hac habitasse platea dictumst. Cras urna quam, malesuada vitae risus at,
-                                        pretium blandit sapien.
+                                        An alternative message for anyone who wants to grow their company profit with technology
                                     </p>
-                                </div>
-                                <div class="col">
-                                    <p class="small mb-0 text-muted">Nec Urna Suscipit Ltd</p>
-                                    <p class="small mb-0 text-muted">P.O. Box 464, 5975 Eget Avenue</p>
-                                    <p class="small mb-0 text-muted">(537) 315-1481</p>
                                 </div>
                             </div>
                         </div>
@@ -41,38 +35,12 @@
                     <hr class="my-4" />
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="firstname">Firstname</label>
-                            <input type="text" id="firstname" class="form-control" placeholder="Brown" />
+                            <label for="firstname">Name</label>
+                            <input type="text" id="name" class="form-control" placeholder="James" />
                         </div>
-                        <div class="form-group col-md-6">
-                            <label for="lastname">Lastname</label>
-                            <input type="text" id="lastname" class="form-control" placeholder="Asher" />
-                        </div>
-                    </div>
                     <div class="form-group">
                         <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="brown@asher.me" />
-                    </div>
-                    <div class="form-group">
-                        <label for="inputAddress5">Address</label>
-                        <input type="text" class="form-control" id="inputAddress5" placeholder="P.O. Box 464, 5975 Eget Avenue" />
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="inputCompany5">Company</label>
-                            <input type="text" class="form-control" id="inputCompany5" placeholder="Nec Urna Suscipit Ltd" />
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputState5">State</label>
-                            <select id="inputState5" class="form-control">
-                                <option selected="">Choose...</option>
-                                <option>...</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-2">
-                            <label for="inputZip5">Zip</label>
-                            <input type="text" class="form-control" id="inputZip5" placeholder="98232" />
-                        </div>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="james@company.com" />
                     </div>
                     <hr class="my-4" />
                     <div class="row mb-4">
@@ -105,3 +73,4 @@
                 </form>
             </div>
         </div>
+@endsection
