3273db67b8b1c492cbb36d6ec262f47ca046f424
Thiago <thirofer@gmail.com>
2023-11-16 20:55:05 -0300

    Fix

--- resources/views/pages/products/pagination.blade.php
+++ resources/views/pages/products/pagination.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Procuts</h1>                    
+        <h1 class="h2">Products</h1>                    
     </div>
     <div>
         <form action="{{ route('products.index') }}" method='get'>
